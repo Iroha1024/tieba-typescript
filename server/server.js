614b1f8ce0e8a759b407f68729716f5e534a0569
Iroha1024 <47475495+Iroha1024@users.noreply.github.com>
2019-08-14 14:27:41 +0800

    Update server.js

--- server/server.js
+++ server/server.js
@@ -13,6 +13,15 @@ const info = require('./routes/info');
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
+app.all('*', function (req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "*");
+    res.header("Access-Control-Allow-Methods", "PUT,POST,GET,DELETE,OPTIONS");
+    res.header("X-Powered-By", ' 3.2.1')
+    res.header("Content-Type", "application/json;charset=utf-8");
+    next();
+});
+
 app.use('/api/login', login);
 app.use('/api/register', register);
 app.use('/api/home', home);
